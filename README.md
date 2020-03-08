4969d3e54cdecbf787f3b347bf4ac4f5595b62af
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2020-03-08 00:50:42 -0300

    - Logger and README.md details

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# shio-cli
+![shio_banner.png](https://shiocms.github.io/shio/img/shio_banner.png) 
+------
+
+**Viglet Shio Client** - Shio CMS command line client to connect to remote Shio CMS and execute actions.
+
+**If you'd like to contribute to Viglet Shio Client, be sure to review the [contribution
+guidelines](CONTRIBUTING.md).**
+
+**We use [GitHub issues](https://github.com/ShioCMS/shio-client/issues) for tracking requests and bugs.**
+
+# Installation
+
+## Download
+
+```shell
+$ git clone https://github.com/ShioCMS/shio-client.git
+$ cd shio-client
+```
+## Deploy
+
+### Build
+Use Go lang to build Shio CMS Client.
+
+```shell
+$ go build shio-cli.go
+```
+
+## Usage
+
+### Connection
+
+**shio-cli.ini** file is present in same directory of shio-client executable.
+Edit this file and add login, password and server URL of your Shio CMS.
+
+```ini
+login="admin"
+password="admin"
+server="http://localhost:2710"
+```
+
+### Command line
+`./shio-cli new name_of_site`: Create a new site
+
+For example
+```shell
+$ ./shio-cli new SampleSite
+```
--- shio-cli.go
+++ shio-cli.go
@@ -9,9 +9,10 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"os"
-	"strconv"
+	"strings"
 
 	"github.com/BurntSushi/toml"
+	"github.com/alexcesaro/log/stdlog"
 )
 
 // Config ...
@@ -21,8 +22,9 @@ type Config struct {
 	Server   string
 }
 
-// ReadConfig: Reads info from config file
+// ReadConfig reads info from config file
 func ReadConfig() Config {
+	logger := stdlog.GetFromFlags()
 	var configfile = "shio-cli.ini"
 	_, err := os.Stat(configfile)
 	if err != nil {
@@ -33,83 +35,72 @@ func ReadConfig() Config {
 	if _, err := toml.DecodeFile(configfile, &config); err != nil {
 		log.Fatal(err)
 	}
-	log.Print(config)
+	logger.Debug(config)
 	return config
 }
 
 func main() {
+	siteName := ""
 	fmt.Println("Starting the application...")
-	config := ReadConfig()
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", config.Server+"/api/v2", nil)
-	req.SetBasicAuth(config.Login, config.Password)
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	//	bodyText, err := ioutil.ReadAll(resp.Body)
 
-	if resp.StatusCode == 200 {
-		fmt.Println("Connected!")
+	if strings.Compare(os.Args[1], "new") == 0 {
+		xsrfToken := ""
+		siteName = os.Args[2]
 
-	} else {
-		fmt.Println("Authentication Failed!")
-	}
-
-	newSite()
-}
-func getCookieByName(cookie []*http.Cookie, name string) string {
-	cookieLen := len(cookie)
-	result := ""
-	for i := 0; i < cookieLen; i++ {
-		if cookie[i].Name == name {
-			result = cookie[i].Value
+		config := ReadConfig()
+		cookieJar, _ := cookiejar.New(nil)
+		client := &http.Client{
+			Jar: cookieJar,
 		}
+		req, err := http.NewRequest("GET", config.Server+"/api/v2", nil)
+		req.SetBasicAuth(config.Login, config.Password)
+		resp, err := client.Do(req)
+
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			xsrfToken = getCookieByName(resp.Cookies(), "XSRF-TOKEN")
+		}
+
+		if resp.StatusCode == 200 {
+			fmt.Println("Connected!")
+
+		} else {
+			fmt.Println("Authentication Failed!")
+		}
+
+		newSite(client, config, xsrfToken, siteName)
+	}
+}
+func getCookieByName(cookies []*http.Cookie, name string) string {
+	logger := stdlog.GetFromFlags()
+	result := ""
+	for _, cookie := range cookies {
+		if cookie.Name == name {
+			result = cookie.Value
+		}
+		logger.Debug("Found a cookie named:", cookie.Name)
+		logger.Debug("Found a cookie value:", cookie.Value)
 	}
 	return result
 }
 
-func newSite() {
-	token1 := ""
-	config := ReadConfig()
-	cookieJar, _ := cookiejar.New(nil)
-	client := &http.Client{
-		Jar: cookieJar,
-	}
-	req, err := http.NewRequest("GET", config.Server+"/api/v2", nil)
-	req.SetBasicAuth(config.Login, config.Password)
-	resp0, err0 := client.Do(req)
+func newSite(client *http.Client, config Config, xsrfToken string, siteName string) {
+	logger := stdlog.GetFromFlags()
+	jsonData := map[string]string{"objectType": "SITE", "name": siteName}
+	jsonValue, _ := json.Marshal(jsonData)
+	req, err := http.NewRequest("POST", config.Server+"/api/v2/site", bytes.NewBuffer(jsonValue))
 
-	if err0 == nil {
-		for _, cookie := range resp0.Cookies() {
-			if cookie.Name == "XSRF-TOKEN" {
-				token1 = cookie.Value
-			}
-			fmt.Println("Found a cookie named:", cookie.Name)
-			fmt.Println("Found a cookie value:", cookie.Value)
-		}
-	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-XSRF-TOKEN", xsrfToken)
+	resp, err := client.Do(req)
 
-	jsonData1 := map[string]string{"objectType": "SITE", "name": "Teste10"}
-	jsonValue1, _ := json.Marshal(jsonData1)
-	req1, err1 := http.NewRequest("POST", config.Server+"/api/v2/site", bytes.NewBuffer(jsonValue1))
-
-	fmt.Println("Err1: " + token1)
-
-	req1.Header.Set("Content-Type", "application/json")
-	req1.Header.Set("X-XSRF-TOKEN", token1)
-	resp1, err1 := client.Do(req1)
-
-	for _, cookie := range req1.Cookies() {
-		fmt.Println("Found a cookie2 named:", cookie.Name)
-		fmt.Println("Found a cookie2 value:", cookie.Value)
-	}
-	if err1 != nil {
+	if err != nil && resp.StatusCode == 200 {
 		fmt.Printf("The HTTP request failed with error %s\n", err)
 	} else {
-		fmt.Printf("Create site was executed: " + strconv.Itoa(resp1.StatusCode))
-		data, _ := ioutil.ReadAll(resp1.Body)
-		fmt.Println("Body" + string(data))
+		fmt.Printf("%s site was created.\n", siteName)
+		data, _ := ioutil.ReadAll(resp.Body)
+		logger.Debug("Body: " + string(data))
 	}
 
 }
